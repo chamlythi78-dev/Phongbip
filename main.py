322e747f2f07a29739a8c41f4d8614baf8184dec
chamlythi78-dev <chamlythi78@gmail.com>
2026-05-04 18:30:55 +0700

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 ADMIN_IDS = [7398112999, 8619503816]
 BOT_USERNAME = "zen88uytins1bot" 
 MIN_WITHDRAW = 200000 
-WIN_RATE = 20 # Tỉ lệ thắng 20%
+WIN_RATE = 90 # Tỉ lệ thắng 90%
 
 # THÔNG TIN NẠP TIỀN
 BANK_INFO = """
